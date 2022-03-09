962bd56960d8d3c2cf6517871819c9e9f0550e4c
Johannes Choo <jhanschoo@gmail.com>
2022-03-09 08:50:34 +0800

    ocaml q1_1

--- 2020/ocaml/q1_1/q1_1.ml
+++ 2020/ocaml/q1_1/q1_1.ml
@@ -6,18 +6,17 @@ let rec acc_inv_2020 next_int set =
   | None -> None
   | Some x ->
     let x_cpl = 2020 - x in
-    if Set.mem set x_cpl then
+    if Set.mem set x then
       Some (x * x_cpl)
     else
       acc_inv_2020 next_int (Set.add set x_cpl)
 
 let next_int_from_stdin () =
-  let line = In_channel.input_line In_channel.stdin in
-  match line with
+  match In_channel.input_line In_channel.stdin with
   | None -> None
   | Some l -> Some (Int.of_string l)
 
 let () =
   match acc_inv_2020 next_int_from_stdin (Set.empty (module Int)) with
   | None -> ()
-  | Some prod -> Out_channel.output_string Out_channel.stdout (Int.to_string prod)
+  | Some prod -> printf "%d\n" prod
