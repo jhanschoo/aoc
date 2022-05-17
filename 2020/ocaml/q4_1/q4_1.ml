b8f86ca1f183be494364f16439a8a4a5a9bc0ad7
Johannes Choo <jhanschoo@gmail.com>
2022-05-17 10:12:38 +0800

    q4_1

---
+++ 2020/ocaml/q4_1/q4_1.ml
@@ -0,0 +1,29 @@
+open Base
+open Stdio
+exception Empty_list
+
+let lines_to_passport_fields lines =
+  let f passports line =
+    match line with
+    | "" -> [] :: passports
+    | s -> match passports with
+            | [] -> raise Empty_list
+            | (passport :: rest) ->
+              (s :: passport) :: rest
+  in
+  lines
+  |> List.fold ~init:[[]] ~f
+  |> List.map ~f:(String.concat ~sep:" ")
+  |> List.map ~f:(String.split ~on:' ')
+
+let filter_cid passport =
+  passport
+  |> List.filter ~f:(fun field -> not (String.is_prefix ~prefix:"cid:" field))
+
+
+let () = Stdio.In_channel.input_lines Stdio.stdin
+    |> lines_to_passport_fields
+    |> List.map ~f:(filter_cid)
+    |> List.filter ~f:(fun passport -> List.length passport >= 7)
+    |> List.length
+    |> printf "%d\n"
