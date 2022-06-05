f394d6a92e30ed3e235afc170a327c748ecdb370
Johannes Choo <jhanschoo@gmail.com>
2022-06-05 16:25:54 +0800

    aoc q16

--- 2020/ocaml/q16_1/q16_1.ml
+++ 2020/ocaml/q16_1/q16_1.ml
@@ -3,23 +3,47 @@ open Core
 
 let rules_regex = Re.Perl.compile_pat "^(.*): (\\d*)-(\\d*) or (\\d*)-(\\d*)$"
 
-let process_rules =
+let parse_rules =
   let f m line =
     let open Result.Let_syntax in
     let%bind groups = Result.of_option ~error:"failed matching" (Re.exec_opt rules_regex line) in
     let%bind key = Result.of_option ~error:"failed extracting key" (Re.Group.get_opt groups 1) in
-    let%bind r1_1 = Result.of_option ~error:"failed extracting r1_1" (Re.Group.get_opt groups 2) in
-    let%bind r1_2 = Result.of_option ~error:"failed extracting r1_2" (Re.Group.get_opt groups 3) in
-    let%bind r2_1 = Result.of_option ~error:"failed extracting r2_1" (Re.Group.get_opt groups 4) in
-    let%bind r2_2 = Result.of_option ~error:"failed extracting r2_2" (Re.Group.get_opt groups 5) in
-    Ok (Map.set m ~key ~data:(r1_1, r1_2))
-    (* Ok (Map.set m ~key ~data:((r1_1, r1_2), (r2_1, r2_2)))  *)
+    let%bind endpoints =
+      List.map [2; 3; 4; 5] ~f:(Re.Group.get_opt groups)
+      |> List.mapi ~f:(fun i s -> Result.of_option s ~error:("failed extracting group" ^ (Int.to_string i)))
+      |> Result.all
+    in
+    let%bind endpoints = endpoints
+      |> List.map ~f:(fun s -> Result.try_with (fun () -> Int.of_string s))
+      |> List.mapi ~f:(fun i s -> Result.map_error s ~f:(Fn.const ("failed converting group" ^ (Int.to_string i))))
+      |> Result.all
+    in
+    Ok (Map.set m ~key ~data:endpoints)
   in
   List.fold_result ~init:(Map.empty (module String)) ~f
 
-let a = (module (struct include Tuple.Make (String) (String) include Tuple.Sexpable (String) (String) end) : Sexpable.S);;
+let parse_ticket ticket =
+  ticket
+  |> String.split ~on:','
+  |> List.map ~f:(fun s -> Result.try_with (fun () -> Int.of_string s))
+  |> List.mapi ~f:(fun i s -> Result.map_error s ~f:(Fn.const ("failed converting field" ^ (Int.to_string i))))
+  |> Result.all
 
-let process_tickets tickets = Ok tickets
+let parse_tickets tickets =
+  tickets
+  |> List.map ~f:parse_ticket
+  |> Result.all
+
+let rule_validats_field field ~rule =
+  match rule with
+  | [r1_1; r1_2; r2_1; r2_2] -> field >= r1_1 && field <= r1_2 || field >= r2_1 && field <= r2_2
+  | _ -> false
+
+let valid_field field ~rules = List.exists (Map.data rules) ~f:(fun rule -> rule_validats_field field ~rule)
+
+(* let valid_ticket ticket ~rules = List.exists ticket ~f:(fun field -> valid_field field ~rules) *)
+
+let invalid_fields_of_ticket ticket ~rules = List.filter ticket ~f:(fun field -> not (valid_field field ~rules))
 
 let process_with_result res =
   let open Result.Let_syntax in
@@ -27,9 +51,11 @@ let process_with_result res =
   | nearby_tickets :: your_ticket :: rules :: _ -> Ok (rules, your_ticket, nearby_tickets)
   | _ -> Error "process_with_result: unexpected result"
   in
-  let%bind rules = process_rules rules and your_ticket = process_tickets your_ticket and nearby_tickets = process_tickets nearby_tickets in
-  let () = printf "%s" (String.t_of_sexp (Map.sexp_of_m__t (module String) (module (Tuple.Sexpable (module String) (module String))) rules)) in
-  Ok (rules, your_ticket, nearby_tickets)
+  let%bind rules = parse_rules rules and _ = parse_tickets your_ticket and nearby_tickets = parse_tickets nearby_tickets in
+  (* let () = printf "%s" (Sexp.to_string (Map.sexp_of_m__t (module String) (List.sexp_of_t (Int.sexp_of_t)) rules)) in *)
+  let invalid_fields_by_ticket = List.map nearby_tickets ~f:(invalid_fields_of_ticket ~rules) in
+  let sum = List.sum (module Int) ~f:Fn.id (List.map invalid_fields_by_ticket ~f:(List.sum (module Int) ~f:Fn.id)) in
+  Ok (printf "%d\n" sum)
 
 let () =
   let res = Stdio.In_channel.input_lines Stdio.stdin in
