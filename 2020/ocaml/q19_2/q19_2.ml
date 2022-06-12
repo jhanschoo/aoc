1d0e1f97c19a466743314b56342d16f739ce794b
Johannes Choo <jhanschoo@gmail.com>
2022-06-12 13:19:48 +0800

    aoc 2020 q19

--- 2020/ocaml/q19_2/q19_2.ml
+++ 2020/ocaml/q19_2/q19_2.ml
@@ -1,15 +1,21 @@
 open Core
+exception Exit
 
 type rule =
   | RExpr of int list list
   | RChar of char
+  | RFinal
   [@@deriving sexp]
 
 let parse_rule line ~rules =
   let i, expr = String.lsplit2_exn line ~on:':' in
   let i = Int.of_string i in
   let rule =
-    if String.(suffix expr 1 = "\"")
+    if Int.(i = 8)
+      then RExpr [[42]; [42; 8]]
+    else if Int.(i = 11)
+      then RExpr [[42; 31]; [42; 11; 31]]
+    else if String.(suffix expr 1 = "\"")
       then RChar (String.get expr (String.length expr - 2))
     else
       RExpr (
@@ -24,32 +30,32 @@ let parse_rule line ~rules =
 
 let rec parse_rules lines ~rules =
   (match lines with
-  | [] -> rules, lines
-  | "" :: rest -> rules, rest
+  | [] -> raise Exit
+  | "" :: rest -> Map.set rules ~key:(-1) ~data:RFinal, rest
   | line :: rest -> parse_rules rest ~rules:(parse_rule line ~rules)
   )
 
-let emp = Set.empty (module Int)
-
-let rec eval_nfa s ~ind ~rind ~len ~rules =
-  if ind >= len then emp else
-  (match Map.find rules rind with
-  | None -> emp
-  | Some (RChar c) ->
-    if Char.(String.get s ind = c) then Set.singleton (module Int) (ind + 1)
-    else emp
-  | Some (RExpr e) ->
-    let f accum ind ~rind = Set.union (eval_nfa s ~ind ~rind ~len ~rules) accum in
-    let f inds rind = Set.fold inds ~init:(Set.empty (module Int)) ~f:(f ~rind) in
-    let f conj = List.fold conj ~init:(Set.singleton (module Int) ind) ~f in
-    Set.union_list (module Int) (List.map e ~f)
+let rec eval_one_or c ~ors ~rules =
+  List.concat_map ors ~f:(eval_one_state c ~rules)
+and eval_one_state c st ~rules =
+  (match st with
+  | i :: rest ->
+    (match Map.find_exn rules i with
+    | RChar t ->
+      if Char.(c = t) then [rest]
+      else []
+    | RExpr expansions ->
+      let f clause = List.concat [clause; rest] in
+      eval_one_or c ~ors:(List.map expansions ~f) ~rules
+    | RFinal -> []
+    )
+  | [] -> []
   )
 
 let sat s ~rules =
-  let len = String.length s in
-  let final = eval_nfa s ~ind:0 ~rind:0 ~len ~rules in
-  Set.mem final len
-
+  let f ors c = eval_one_or c ~ors ~rules in
+  let finals = String.fold s ~init:[[0;-1]] ~f in
+  List.mem finals [-1] ~equal:(List.equal Int.equal)
 
 let () =
   let res = Stdio.In_channel.input_lines Stdio.stdin in
