aa639b506770713e2c5a3ce2241fcc974d50ab63
Johannes Choo <jhanschoo@gmail.com>
2022-06-12 10:10:43 +0800

    add 2020q15

---
+++ 2020/ocaml/q15_2/q15_2.ml
@@ -0,0 +1,42 @@
+open Core
+
+let compute_diff e ~t ~clock =
+  (match Map.find clock e with
+  | Some n -> (t - n)
+  | None -> 0
+  )
+
+let rec process_initial initial ~t ~clock =
+  (match initial with
+  | [] -> Error ()
+  | [i] -> Ok (t + 1, Map.set clock ~key:i ~data:t, compute_diff i ~t ~clock)
+  | i :: rest -> process_initial rest ~t:(t + 1) ~clock:(Map.set clock ~key:i ~data:t)
+  )
+
+let rec compute ~t ~clock ~next =
+  match t with
+  | 30_000_000 -> next
+  | _ ->
+    let next_next =
+      match Map.find clock next with
+      | Some n -> (t - n)
+      | None -> 0
+    in
+    compute ~t:(t + 1) ~clock:(Map.set clock ~key:next ~data:t) ~next:next_next
+
+let () =
+  let res = Stdio.In_channel.input_lines Stdio.stdin in
+  let _ = (
+    let open Result.Let_syntax in
+    let%bind l =
+    match res with
+    | [l] -> Ok l
+    | _ -> Error ()
+    in
+    let initial = List.map (String.split l ~on:',') ~f:Int.of_string in
+    let%bind (t, clock, next) = process_initial initial ~t:1 ~clock:(Map.empty (module Int)) in
+    let next = compute ~t ~clock ~next in
+    Ok (printf "%d\n" next)
+  )
+  in
+  ()
---
+++ 2020/ocaml/q19_1/q19_1.ml
@@ -0,0 +1,6 @@
+open Core
+
+
+let () =
+  let _ = Stdio.In_channel.input_lines Stdio.stdin in
+  ()
