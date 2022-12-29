57ad0381b879f4e131eb9378e9f42e63e11ecd7b
Johannes Choo <jhanschoo@gmail.com>
2022-12-29 23:13:24 +0800

    refactor cxx/2022/12.1

--- cxx/2022/12.1/main.cxx
+++ cxx/2022/12.1/main.cxx
@@ -1,24 +1,17 @@
-#include <algorithm>
-#include <ranges>
-#include <vector>
-#include <string>
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
 
 int main() {
-    using ll = long long;
-    std::vector <std::string> heightmap;
-    std::string line;
+    auto heightmap = std::vector <std::string>{};
+    auto line = std::string{};
     while (std::cin >> line) {
         heightmap.push_back(line);
     }
-    for (auto &&row: heightmap) {
-        std::cout << row << std::endl;
-    }
+    auto num_maj = heightmap.size();
+    auto num_min = heightmap[0].size();
     auto start = std::pair < size_t, size_t>{ 0, 0 };
     auto goal = std::pair < size_t, size_t>{ 0, 0 };
-    for (size_t i = 0; i < heightmap.size(); ++i) {
-        for (size_t j = 0; j < heightmap[i].size(); ++j) {
+    for (size_t i = 0; i < num_maj; ++i) {
+        for (size_t j = 0; j < num_min; ++j) {
             if (heightmap[i][j] == 'S') {
                 start = {i, j};
                 heightmap[i][j] = 'a';
@@ -38,7 +31,6 @@ int main() {
     while (!frontier.empty()) {
         auto new_frontier = std::vector < std::pair < size_t, size_t>>{};
         for (auto &&p: frontier) {
-            std::cout << p.first << " " << p.second << std::endl;
             if (p == goal) {
                 std::cout << steps << std::endl;
                 return 0;
@@ -47,19 +39,19 @@ int main() {
                 continue;
             }
             seen.insert(p);
-            auto [i, j] = p;
+            auto& [i, j] = p;
             auto current = heightmap[i][j];
             if (i > 0 && heightmap[i - 1][j] - current <= 1) {
-                new_frontier.push_back({i - 1, j});
+                new_frontier.emplace_back(i - 1, j);
             }
-            if (i < heightmap.size() - 1 && heightmap[i + 1][j] - current <= 1) {
-                new_frontier.push_back({i + 1, j});
+            if (i < num_maj - 1 && heightmap[i + 1][j] - current <= 1) {
+                new_frontier.emplace_back(i + 1, j);
             }
             if (j > 0 && heightmap[i][j - 1] - current <= 1) {
-                new_frontier.push_back({i, j - 1});
+                new_frontier.emplace_back(i, j - 1);
             }
-            if (j < heightmap[i].size() - 1 && heightmap[i][j + 1] - current <= 1) {
-                new_frontier.push_back({i, j + 1});
+            if (j < num_min - 1 && heightmap[i][j + 1] - current <= 1) {
+                new_frontier.emplace_back(i, j + 1);
             }
         }
         frontier = new_frontier;
