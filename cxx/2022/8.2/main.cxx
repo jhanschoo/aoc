48597dfaae025d138af47a0426f95e1b109ff551
Johannes Choo <jhanschoo@gmail.com>
2022-12-29 22:20:44 +0800

    refactor cxx/2022/8.2

--- cxx/2022/8.2/main.cxx
+++ cxx/2022/8.2/main.cxx
@@ -1,101 +1,75 @@
 #include <bits/stdc++.h>
 
 int main() {
-	std::vector<std::string> treemap;
+	std::vector<std::string> chart_strs;
 	std::string line;
 	while (std::cin >> line) {
-		treemap.push_back(line);
+		chart_strs.push_back(line);
 	}
-	auto num_rows = treemap.size();
-	auto num_cols = treemap[0].size();
-	std::vector<std::vector<int>> scenic_left, scenic_right, scenic_up, scenic_down;
-	for (size_t i = 0; i < num_rows; ++i) {
-		scenic_left.push_back(std::vector<int>(num_cols, -1));
-		scenic_right.push_back(std::vector<int>(num_cols, -1));
-	}
-	for (size_t j = 0; j < num_cols; ++j) {
-		scenic_up.push_back(std::vector<int>(num_rows, -1));
-		scenic_down.push_back(std::vector<int>(num_rows, -1));
-	}
-	for (size_t i = 0; i < num_rows; ++i) {
-		auto decreasing = std::vector<std::pair<size_t, char>>{};
-		for (size_t j = 0; j < num_cols; ++j) {
-			auto& score_left = scenic_left[i][j];
-			score_left = j;
-			for (auto it = decreasing.begin(); it != decreasing.end(); ++it) {
-				const auto& [index, height] = *it;
-				if (height >= treemap[i][j]) {
-					score_left = j - index;
-				} else {
-					break;
-				}
-			}
-			while (!decreasing.empty() && decreasing.back().second <= treemap[i][j]) {
-				decreasing.pop_back();
-			}
-			decreasing.push_back({j, treemap[i][j]});
-		}
-		decreasing.resize(0);
-		for (size_t j_cpl = 0; j_cpl < num_cols; ++j_cpl) {
-			auto j = num_cols - j_cpl - 1;
-			auto& score_right = scenic_right[i][j];
-			score_right = j_cpl;
-			for (auto it = decreasing.begin(); it != decreasing.end(); ++it) {
-				const auto& [index, height] = *it;
-				if (height >= treemap[i][j]) {
-					score_right = index - j;
-				} else {
-					break;
-				}
-			}
-			while (!decreasing.empty() && decreasing.back().second <= treemap[i][j]) {
-				decreasing.pop_back();
-			}
-			decreasing.push_back({j, treemap[i][j]});
-		}
-	}
-	for (size_t j = 0; j < num_cols; ++j) {
-		auto decreasing = std::vector<std::pair<size_t, char>>{};
-		for (size_t i = 0; i < num_rows; ++i) {
-			auto& score_up = scenic_up[i][j];
-			score_up = i;
-			for (auto it = decreasing.begin(); it != decreasing.end(); ++it) {
-				const auto& [index, height] = *it;
-				if (height >= treemap[i][j]) {
-					score_up = i - index;
-				} else {
-					break;
-				}
-			}
-			while (!decreasing.empty() && decreasing.back().second <= treemap[i][j]) {
-				decreasing.pop_back();
-			}
-			decreasing.push_back({i, treemap[i][j]});
-		}
-		decreasing.resize(0);
-		for (size_t i_cpl = 0; i_cpl < num_rows; ++i_cpl) {
-			auto i = num_rows - i_cpl - 1;
-			auto& score_down = scenic_down[i][j];
-			score_down = i_cpl;
-			for (auto it = decreasing.begin(); it != decreasing.end(); ++it) {
-				const auto& [index, height] = *it;
-				if (height >= treemap[i][j]) {
-					score_down = index - i;
-				} else {
-					break;
-				}
-			}
-			while (!decreasing.empty() && decreasing.back().second <= treemap[i][j]) {
-				decreasing.pop_back();
-			}
-			decreasing.push_back({i, treemap[i][j]});
-		}
-	}
-	int max = 0;
-	for (size_t i = 0; i < num_rows; ++i) {
-		for (size_t j = 0; j < num_cols; ++j) {
-			max = std::max(max, scenic_left[i][j] * scenic_right[i][j] * scenic_up[i][j] * scenic_down[i][j]);
-		}
-	}
-	std::cout << max << std::endl;
+	auto num_rows = chart_strs.size();
+	auto num_cols = chart_strs[0].size();
+    auto cells = num_rows * num_cols;
+
+    auto chart = std::valarray<char>(num_rows * num_cols);
+    for (std::size_t i = 0; i < num_rows; ++i) {
+        for (std::size_t j = 0; j < num_cols; ++j) {
+            chart[i * num_cols + j] = chart_strs[i][j];
+        }
+    }
+
+    auto regimes = std::array{
+        // row-wise ltr
+        std::tuple{
+            num_rows, // num_maj
+            num_cols, // num_min
+            +[](std::size_t num_rows, std::size_t num_cols, std::size_t maj, std::size_t min) { return maj * num_cols + min; },
+            std::valarray(std::size_t{}, cells)
+        },
+        // row-wise rtl
+        std::tuple{
+            num_rows,
+            num_cols,
+            +[](std::size_t num_rows, std::size_t num_cols, std::size_t maj, std::size_t min) { return maj * num_cols + (num_cols - min - 1); },
+            std::valarray(std::size_t{}, cells)
+        },
+        // col-wise utd
+        std::tuple{
+            num_cols,
+            num_rows,
+            +[](std::size_t num_rows, std::size_t num_cols, std::size_t maj, std::size_t min) { return maj + min * num_cols; },
+            std::valarray(std::size_t{}, cells)
+        },
+        // col-wise dtu
+        std::tuple{
+            num_cols,
+            num_rows,
+            +[](std::size_t num_rows, std::size_t num_cols, std::size_t maj, std::size_t min) { return maj + (num_rows - min - 1) * num_cols; },
+            std::valarray(std::size_t{}, cells)
+        },
+    };
+
+    for (auto&& [num_maj, num_min, abs_map, scenic] : regimes) {
+        for (std::size_t maj = 0; maj < num_maj; ++maj) {
+            auto decreasing = std::vector<std::pair<std::size_t, char>>{};
+            for (std::size_t min = 0; min < num_min; ++min) {
+                auto abs_i = abs_map(num_rows, num_cols, maj, min);
+                auto current_height = chart[abs_i];
+                auto& score = scenic[abs_i];
+                score = min;
+                for (const auto& [index, height] : decreasing) {
+                    if (height >= current_height) {
+                        score = min - index;
+                    } else {
+                        break;
+                    }
+                }
+                while (!decreasing.empty() && decreasing.back().second <= current_height) {
+                    decreasing.pop_back();
+                }
+                decreasing.emplace_back(min, current_height);
+            }
+        }
+    }
+
+	std::cout << (std::get<3>(regimes[0]) * std::get<3>(regimes[1]) * std::get<3>(regimes[2]) * std::get<3>(regimes[3])).max() << std::endl;
 }
