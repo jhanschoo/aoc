5b33c659e6bb5a2ee97158a84a1fbc0b5ce0a4c5
Johannes Choo <jhanschoo@gmail.com>
2023-01-08 17:09:02 +0800

    cxx/2022/12.2

--- cxx/2022/12.2/main.cxx
+++ cxx/2022/12.2/main.cxx
@@ -9,15 +9,13 @@ int main() {
     auto num_maj = heightmap.size();
     auto num_min = heightmap[0].size();
     auto start = std::pair < size_t, size_t>{ 0, 0 };
-    auto goal = std::pair < size_t, size_t>{ 0, 0 };
     for (size_t i = 0; i < num_maj; ++i) {
         for (size_t j = 0; j < num_min; ++j) {
             if (heightmap[i][j] == 'S') {
-                start = {i, j};
                 heightmap[i][j] = 'a';
             }
             if (heightmap[i][j] == 'E') {
-                goal = {i, j};
+                start = {i, j};
                 heightmap[i][j] = 'z';
             }
         }
@@ -31,7 +29,8 @@ int main() {
     while (!frontier.empty()) {
         auto new_frontier = std::vector < std::pair < size_t, size_t>>{};
         for (auto &&p: frontier) {
-            if (p == goal) {
+            auto& [i, j] = p;
+            if (heightmap[i][j] == 'a') {
                 std::cout << steps << std::endl;
                 return 0;
             }
@@ -39,18 +38,17 @@ int main() {
                 continue;
             }
             seen.insert(p);
-            auto& [i, j] = p;
             auto current = heightmap[i][j];
-            if (i > 0 && heightmap[i - 1][j] - current <= 1) {
+            if (i > 0 && heightmap[i - 1][j] - current >= -1) {
                 new_frontier.emplace_back(i - 1, j);
             }
-            if (i < num_maj - 1 && heightmap[i + 1][j] - current <= 1) {
+            if (i < num_maj - 1 && heightmap[i + 1][j] - current >= -1) {
                 new_frontier.emplace_back(i + 1, j);
             }
-            if (j > 0 && heightmap[i][j - 1] - current <= 1) {
+            if (j > 0 && heightmap[i][j - 1] - current >= -1) {
                 new_frontier.emplace_back(i, j - 1);
             }
-            if (j < num_min - 1 && heightmap[i][j + 1] - current <= 1) {
+            if (j < num_min - 1 && heightmap[i][j + 1] - current >= -1) {
                 new_frontier.emplace_back(i, j + 1);
             }
         }
