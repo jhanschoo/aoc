6777c2ada926c795007ee6293ee85d60ab2999b7
Johannes Choo <jhanschoo@gmail.com>
2023-02-19 11:06:55 +0800

    rust days 1 and 2
    Signed-off-by: Johannes Choo <jhanschoo@gmail.com>

--- rust/src/year_2022/day_1/mod.rs
+++ rust/src/year_2022/day_1/mod.rs
@@ -1,35 +1,78 @@
-use std::io::{BufRead, Error, Write};
+use std::{io::{BufRead, Error}, cmp};
 
-pub fn day_1_1(input: &mut impl BufRead, output: &mut impl Write) -> Result<(), Error> {
+pub fn day_1_1(input: &mut impl BufRead) -> Result<String, Error> {
     let (tentative_max, curr) = input
         .lines()
-        .fold((0u64, 0u64), |(max_cal, curr_cal), l| {
-            let l = l.unwrap();
+        .filter_map(Result::ok)
+        .fold((0, 0), |(max_cal, curr_cal), l| {
             if l.is_empty() {
                 (max_cal.max(curr_cal), 0)
             } else {
-                (max_cal, curr_cal + l.parse::<u64>().unwrap())
+                (max_cal, curr_cal + l.parse::<i32>().unwrap())
             }
         });
-    let max_cal = tentative_max.max(curr);
-    writeln!(output, "{max_cal}")?;
-    Ok(())
+    let tentative_max = tentative_max.max(curr);
+    Ok(format!("{tentative_max}"))
+}
+
+pub fn day_1_2(input: &mut impl BufRead) -> Result<String, Error> {
+    let mut maximums = vec![0; 3];
+    let mut curr = 0;
+    let mut update_maximums = |v| {
+        let max_elem = maximums.iter_mut().min().unwrap();
+        *max_elem = cmp::max(*max_elem, v);
+    };
+
+    input
+        .lines()
+        .filter_map(Result::ok)
+        .for_each(|l| {
+            if l.is_empty() {
+                update_maximums(curr);
+                curr = 0;
+            } else {
+                curr += l.parse::<i32>().unwrap();
+            }
+        });
+    update_maximums(curr);
+    let sum_cal: i32 = maximums.iter().sum();
+    Ok(format!("{sum_cal}"))
 }
 
 #[cfg(test)]
 mod test {
-    use std::{io::{self, BufReader}, fs::File};
+    use std::{io::{self, BufReader, Write}, fs::File};
     #[test]
     fn diagnostic_day_1_1() {
         let input = File::open("src/year_2022/day_1/diagnostic.txt").unwrap();
         let mut input = BufReader::new(input);
-        super::day_1_1(&mut input, &mut io::stdout()).unwrap();
+        let res = super::day_1_1(&mut input).unwrap();
+        assert_eq!(res, "24000");
+        writeln!(io::stdout(), "{}", res).unwrap();
     }
 
     #[test]
     fn test_day_1_1() {
         let input = File::open("src/year_2022/day_1/input.txt").unwrap();
         let mut input = BufReader::new(input);
-        super::day_1_1(&mut input, &mut io::stdout()).unwrap();
+        let res = super::day_1_1(&mut input).unwrap();
+        writeln!(io::stdout(), "{}", res).unwrap();
+    }
+
+    #[test]
+    fn diagnostic_day_1_2() {
+        let input = File::open("src/year_2022/day_1/diagnostic.txt").unwrap();
+        let mut input = BufReader::new(input);
+        let res = super::day_1_2(&mut input).unwrap();
+        assert_eq!(res, "45000");
+        writeln!(io::stdout(), "{}", res).unwrap();
+    }
+
+    #[test]
+    fn test_day_1_2() {
+        let input = File::open("src/year_2022/day_1/input.txt").unwrap();
+        let mut input = BufReader::new(input);
+        let res = super::day_1_2(&mut input).unwrap();
+        writeln!(io::stdout(), "{}", res).unwrap();
     }
 }
